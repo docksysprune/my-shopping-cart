a1f9d5e51d432324aa8ae12ba0d599ec7b3f0286
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2023-10-16 01:26:05 -0600

    Functions on the main page.

--- src/main.js
+++ src/main.js
@@ -2,30 +2,38 @@ let shop = document.getElementById("grid-items");
 
 let shopItemsData = [
   {
-    id: "001",
+    id: 001,
     name: "casual shirt",
     price: 45,
     img: "img-1.jpg",
   },
   {
-    id: "002",
+    id: 002,
     name: "casual shirt",
     price: 45,
     img: "img-2.jpg",
   },
   {
-    id: "003",
+    id: 003,
     name: "casual shirt",
     price: 45,
     img: "img-3.jpg",
   },
 ];
 
+let basket = [
+  {
+    id: 001,
+    item: 1,
+  },
+];
+
 console.log(shop);
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData.map((item)=>{
-    return `
+  return (shop.innerHTML = shopItemsData
+    .map((item) => {
+      return `
     <div id=product-id-${item.id} class="col">
             <div class="card shadow-sm">
               <img src="./images/${item.img}" alt="" class="card-img" />
@@ -36,24 +44,77 @@ let generateShop = () => {
                     <button
                       type="button"
                       class="btn btn-sm btn-outline-secondary"
+                      onclick="increment(${item.id})"
                     >
                       +
                     </button>
-                    <small class="text-body-secondary">0</small>
+                    <small id=${item.id} class="text-body-secondary quantity">0</small>
                     <button
                       type="button"
                       class="btn btn-sm btn-outline-secondary"
+                      onclick="decrement(${item.id})"
                     >
                       -
                     </button>
                   </div>
-                  <div class="text-body-secondary">${item.price}</div>
+                  <div class="text-body-secondary">$${item.price}</div>
                 </div>
               </div>
             </div>
           </div>
     `;
-  }).join(""));
+    })
+    .join(""));
 };
 
 generateShop();
+
+let increment = (id) => {
+  incrementBasket(id);
+  console.log(basket);
+  update(id);
+};
+
+let decrement = (id) => {
+  decrementBasket(id);
+  console.log(basket);
+  update(id);
+};
+
+let incrementBasket = (id) => {
+  let search = basket.find((x) => x.id === id);
+  if (search === undefined) {
+    basket.push({
+      id: id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+};
+
+let decrementBasket = (id) => {
+  if (basket) {
+    let search = basket.find((x) => x.id === id);
+    if (search) {
+      let res = --search.item;
+      if (res < 0) {
+        search.item = 0;
+      } else {
+        search.item = res;
+      }
+    }
+  }
+};
+
+let update = (id) => {
+  if(basket) {
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    console.log("Update function is running");
+  }  
+}
+
+let calculation = () => {
+  
+}
